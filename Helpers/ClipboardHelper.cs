81b0062846dc899f79d2766a39cf791766d7c9fc
Jefferson Ortiz Rodriguez <jortizr@ut.edu.co>
2026-05-24 23:09:57 -0500

    Se creo el helper reutilizable para usar los datos del portapapeles en las secciones.

---
+++ Helpers/ClipboardHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperacionTools.Helpers
+{
+    internal class ClipboardHelper
+    {
+        /// <summary>
+        /// Obtiene el texto del portapapeles y lo divide en líneas eliminando las vacías.
+        /// </summary>
+        public static IEnumerable<string> ObtenerLineasDesdePortapapeles()
+        {
+            string texto = System.Windows.Clipboard.GetText();
+            if(string.IsNullOrEmpty(texto))
+                return Array.Empty<string>();
+
+            return texto.Split(new[] { Environment.NewLine, "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
