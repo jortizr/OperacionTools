385a9f3e682e49f88e70c3d54e8dc3aadc853b5c
Jefferson Ortiz Rodriguez <jortizr@ut.edu.co>
2026-05-24 23:33:31 -0500

    se crea la clase que maneja la configuracion de red para conectarse al servidor remoto o de red local, donde los datos persisten.

---
+++ Helpers/ConfiguracionRed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperacionTools.Helpers
+{
+    internal class ConfiguracionRed
+    {
+        public string RutaServidor { get; set; } = @"\\SERVIDOR-PC\Compartido\InventarioData\";
+        public string UsuarioRed { get; set; } = "";
+        public string ContrasenaRed { get; set; } = "";
+        public bool UtilizarRutaRed { get; set; } = true;
+    }
+}
