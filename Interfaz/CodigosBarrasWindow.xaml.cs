a478c47944a1c0435b7d9a460f0ac4a985de08cd
Jefferson Ortiz Rodriguez <jortizr@ut.edu.co>
2026-05-30 10:09:53 -0500

    ajuste de importacion de clases

--- Interfaz/CodigosBarrasWindow.xaml.cs
+++ Interfaz/CodigosBarrasWindow.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Runtime.CompilerServices;
-using System.Windows.Input;
-using System.ComponentModel;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using static OperacionTools.Interfaz.GenerarGuiaView;
 
 namespace OperacionTools.Interfaz
@@ -96,8 +87,8 @@ namespace OperacionTools.Interfaz
         }
 
         //notificar cambios en las propiedades para actualizar la interfaz
-        public event PropertyChangedEventHandler ?PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
