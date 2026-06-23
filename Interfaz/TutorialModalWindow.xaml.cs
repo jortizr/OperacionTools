8e9497630b5620f85895453fe0f9ee3cbef5c3cb
Jefferson Ortiz Rodriguez <jortizr@ut.edu.co>
2026-06-23 17:15:17 -0500

    Mejoras en tutoriales y descripciones de módulos
    Se agregan casos al switch para los módulos "generador código barras", "digitador guías" e "historial inventario", incluyendo iconos, títulos, descripciones y pasos detallados. Se mejora la descripción y los pasos del módulo "configuración" para mayor claridad y especificidad. Ahora cada módulo cuenta con descripciones ampliadas y pasos numerados y explicados con mayor precisión.

--- Interfaz/TutorialModalWindow.xaml.cs
+++ Interfaz/TutorialModalWindow.xaml.cs
@@ -51,9 +51,37 @@ namespace OperacionTools.Interfaz
                 case "configuracion":
                     TxtIcono.Text = "⚙️";
                     TxtTituloModulo.Text = "Configuración de Red";
-                    pasos.Add(new PasoTutorial { Numero = 1, Titulo = "Activa el Almacenamiento", Descripcion = "Marca la casilla para habilitar las copias automáticas en el servidor corporativo." });
-                    pasos.Add(new PasoTutorial { Numero = 2, Titulo = "Ruta UNC", Descripcion = "Valida que la dirección IP y la carpeta compartida apunten al servidor correcto de Envía." });
-                    pasos.Add(new PasoTutorial { Numero = 3, Titulo = "Credenciales", Descripcion = "Digita el usuario de dominio y contraseña asignados para que el software tenga permisos de escritura." });
+                    TxtDescripcionModulo.Text = "Este módulo sirve para configurar una carpeta compartida centralizada, donde se almacenaran los registros de los inventarios para consultas posteriores y almacenamiento de autoguardado. Por defecto se almacena en el disco duro.";
+                    pasos.Add(new PasoTutorial { Numero = 1, Titulo = "Paso 1: Activar el Almacenamiento", Descripcion = "Marca la casilla para habilitar las copias automáticas en el servidor corporativo LAN." });
+                    pasos.Add(new PasoTutorial { Numero = 2, Titulo = "Paso 2: Agregar Ruta UNC", Descripcion = "Valida que la dirección IP y la carpeta compartida apunten al servidor correcto de Envía y/o PC dedicado." });
+                    pasos.Add(new PasoTutorial { Numero = 3, Titulo = "Paso 3: Agregar Credenciales", Descripcion = "Digita el usuario de dominio y contraseña del equipo donde se creo la carpeta compartida." });
+                    break;
+
+                case "generador codigo barras":
+                    TxtIcono.Text = "🧾";
+                    TxtTituloModulo.Text = "Generador Cod. Barras";
+                    TxtDescripcionModulo.Text = "Este módulo sirve para generar los codigos de barras en base a las guias copiadas en formato tabulacion o la guia unida. Util para leer desde la pantalla los codigos de barras.";
+                    pasos.Add(new PasoTutorial { Numero = 1, Titulo = "Paso 1: Copiado de guias", Descripcion = "Copia las guias y pegalas con el boton 'Pegar Portapapeles' ya sea en formato (1-1-1) o (012345678901)." });
+                    pasos.Add(new PasoTutorial { Numero = 2, Titulo = "Paso 2: Generar Cod. de barras", Descripcion = "Presiona el boton 'Generar Cod. Barras' el cual genera la lista con las guias copiadas en una ventana segundaria." });
+                    pasos.Add(new PasoTutorial { Numero = 3, Titulo = "Paso 3: Imprimir Cod. Barras", Descripcion = "Presiona el boton 'Imprimir listado' donde podra visualizar la ventana de impresion donde selecciona la impresora a usaar." });
+                    break;
+
+                case "digitador guias":
+                    TxtIcono.Text = "⚙️";
+                    TxtTituloModulo.Text = "Digitador de guias";
+                    TxtDescripcionModulo.Text = "Este módulo sirve para transcribir un listado de guias a una aplicacion externa que tenga 3 campos donde se ingrese este informacion. Util para replanillar guias de contratistas y/o contingencia para planillar desde una lista de excel.";
+                    pasos.Add(new PasoTutorial { Numero = 1, Titulo = "Paso 1: Copiar guias", Descripcion = "Copiar el listado de guias y presionar el boton 'Pegar Portapapeles' se almacenaran las guias en un listado, ya formateadas, en caso de inconsistente seleccionar y presionar 'supr' desde el teclado." });
+                    pasos.Add(new PasoTutorial { Numero = 2, Titulo = "Paso 2: Ajustar tiempo", Descripcion = "Selecciona la barra de tiempo en milisegundos para ajustar la velocidad de digitación." });
+                    pasos.Add(new PasoTutorial { Numero = 3, Titulo = "Paso 3: Ejecutar digitación", Descripcion = "Al presionar el boton 'Iniciar Digitación' tendra 5 segundo para seleccionar la aplicacion y seleccionar el campo donde se ingresan las guias, se debe dejar quieto mientras finaliza el proceso." });
+                    break;
+
+                case "historial inventario":
+                    TxtIcono.Text = "📒";
+                    TxtTituloModulo.Text = "Historial de inventarios";
+                    TxtDescripcionModulo.Text = "Este módulo muestra el listado de los inventarios realizaados en ese equipo, en caso de ver los almacenados en el servidor local, debe configurar primero el servidor en la pestaña configuración. Se podra filtrar y visualizar los inventarios realizados e imprimirlos nuevamente.";
+                    pasos.Add(new PasoTutorial { Numero = 1, Titulo = "Paso 1: Seleccionar rango de fecha", Descripcion = "Selecciona el rango de fecha desde los dos campos para determinar un rango y poder filtar el historial deseado." });
+                    pasos.Add(new PasoTutorial { Numero = 2, Titulo = "Paso 2: Visualizar un registro", Descripcion = "En el listado de registros presionar el boton 'Ver Reporte', aparecera una ventana donde se podra visualizar el reporte generado." });
+                    pasos.Add(new PasoTutorial { Numero = 3, Titulo = "Paso 3: Imprimir o Guardar", Descripcion = "Seleccione el boton de 'Imprimir' donde podra reimprimir el reporte o guardarlo como PDF." });
                     break;
 
                 default:
